5f6ed5d378244c38d87de19edcaff7931ade8313
Eleven <nieshiyi20060927@qq.com>
2026-09-02 16:22:14 +0800

    DOM修改样式

--- Web-APIs/Chapter03-DOM.js
+++ Web-APIs/Chapter03-DOM.js
@@ -54,7 +54,7 @@ li2.push("这是一个新的li标签");
 // 1.修改DOM对象的内容
 // 对象.innerHTML: 获取或设置标签的内容（解析标签！！！）
 // 对象.innerText: 获取或设置标签的内容（不解析标签！！！）
-const box = document.querySelector("#box > p");
+const box = document.querySelector("#box>p");
 console.log(box.innerText);        // 获取标签的内容（不包括标签）
 box.innerText = "这是修改后的内容xxxx,原来的内容请见控制台打印";  // 修改标签的内容（不包括标签）
 
@@ -63,11 +63,46 @@ console.log(box2.innerHTML);      // 获取标签的内容（包括标签）
 box2.innerHTML = "<strong>这是修改后的内容xxxx,原来的内容请见控制台打印</strong>";  //可以解析标签：<strong>标签加粗</strong>
 
 
-
 //========================================操作元素属性========================================
-// 1.获取DOM对象的属性
-// 对象.getAttribute("属性名"): 获取DOM对象的属性值
+// 对象.属性 = '值';
+// 1. 通过 style 属性来修改元素的样式
+// 2. 通过 className 属性来修改元素的类名
+// 3. 通过 classList 属性来修改元素的类名（可以添加、删除、切换类名）
+// ------   ----  ---- -----   ---   ---  ----  --- --
+// 4. 通过 id 属性来修改元素的id
+// 5. 通过 src 属性来修改元素的图片路径
+// 6. 通过 href 属性来修改元素的超链接路径
+// 7. 通过 title 属性来修改元素的提示信息
+// 8. 通过 alt 属性来修改元素的图片替代文本
+// 9. 通过 value 属性来修改元素的值
+
+
+// （一）=======================style属性=======================
+// 对象.style.样式属性 = '值';
+const boxMain = document.querySelector("#box");
 const box3 = document.querySelector("#box p:nth-child(3)");
-console.log(box3.getAttribute("class"));   // 获取class属性的值
-console.log(box3.getAttribute("id"));      // 获取id属性的值
-console.log(box3.getAttribute("style"));   // 获取style属性的值
+box3.style.color = "red";      // 修改字体颜色
+//在CSS里字体大小是font-size，在JS里是fontSize，JS里是驼峰命名法
+box3.style.fontSize = "150px";  // 修改字体大小(别忘记带css单位：px)
+//在CSS里背景颜色是background-color，在JS里是backgroundColor，JS里是驼峰命名法
+boxMain.style.backgroundColor = "pink";  // 修改box大容器的背景颜色
+box3.style.backgroundColor = "yellow";   // 修改第三个段落p的背景颜色
+
+
+//（二）=======================className属性=======================
+// 在css里写好样式
+// 在JS里通过className来修改元素的类名：className会覆盖原来的类名
+// 为了不覆盖原来的类名，可以使用classList.add()来添加（追加）类名
+const box4 = document.querySelector("#box p:nth-child(4)");
+box4.className = "box4";  // 修改第四个段落p的类名为box4
+
+
+// （三）=======================classList属性=======================
+// 对象.classList.add('类名'): 添加（追加）类名
+// 对象.classList.remove('类名'): 删除类名
+// 对象.classList.toggle('类名'): 切换类名（有就删除，没有就添加）
+// 类名前不加.
+const box5 = document.querySelector("#box p:nth-child(5)");
+// box5.classList.add("box4");
+// box5.classList.remove("box4");
+box5.classList.toggle("box4");  // 切换类名（有就删除，没有就添加）
