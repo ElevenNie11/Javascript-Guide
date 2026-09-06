f8e4cffd9f92e3a5ac991803c556de7e33c5063c
Eleven <nieshiyi20060927@qq.com>
2026-09-06 22:47:29 +0800

    定时器函数

--- 实战练习/练习03-学生随机点名/random.js
+++ 实战练习/练习03-学生随机点名/random.js
@@ -11,7 +11,7 @@ startButton.addEventListener('click', () => {
 
     // 每间隔100ms显示一个名字
     const timer = setInterval(() => {
-        // 随机生成一个数组下标
+    // 随机生成一个数组下标
     const randomIndex = Math.floor(Math.random() * student.length);
     // 获取学生姓名
     const randomName = student[randomIndex];
@@ -27,3 +27,29 @@ startButton.addEventListener('click', () => {
         startButton.disabled = false; // 启用
     }, 3000);
 });
+
+
+
+// 定时器函数
+// setInterval() 和 setTimeout(函数， 时间)
+// 1. setInterval(): 每隔一段时间反复执行   （反复）
+// 2. setTimeout():  过一段时间以后执行一次 （一次）
+
+
+// setTimeout(() => {
+//     console.log("你好");
+// }, 3000);
+// 上面这段函数表示：3秒以后，执行这个函数（只执行一次）
+
+
+// setInterval(() => {
+//     console.log("你好");
+// }, 1000);
+// 每隔一秒执行一次
+
+// Q：为什么setInterval要配合clearInterval？
+// A：因为setInterval不会自己停下来，我们需要告诉JS现在可以停下来了
+// setInterval会返回一个东西，我们把它保存到const timer这个常量里
+
+
+// 注意事项：function表示这个函数本身。而function()表示现在立刻执行这个函数
