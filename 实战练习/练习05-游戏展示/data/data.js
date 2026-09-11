696c5db3d807661bcda7c87a699bb1c2c1fb9c9b
Eleven <nieshiyi20060927@qq.com>
2026-09-12 00:34:51 +0800

    JS01

---
+++ 实战练习/练习05-游戏展示/data/data.js
@@ -0,0 +1,25 @@
+// 游戏数据（对象数组）
+const games = [
+    {
+        id: 1,
+        workName: "Minecraft",
+        category:"沙盒",
+        price: 90,
+        description:"自由建造和探索世界。"
+    },
+    {
+        id: 2,
+        workName: "Elden Ring",
+        category:"动作角色扮演",
+        price: 298,
+        description:"探索未知世界，与强敌战斗。"
+    },
+    {
+        
+        id: 1,
+        workName: "Stardew Valley",
+        category:"模拟经营",
+        price: 45,
+        description:"又来锄地了。"
+    }
+];
--- 实战练习/练习05-游戏展示/gamehub.js
+++ 实战练习/练习05-游戏展示/gamehub.js
@@ -5,3 +5,24 @@
 // JavaScript生成游戏卡片
 //  ↓
 // 放到HTML页面里去
+
+// 获取游戏列表容器
+const gameList = document.querySelector('#game-list');
+// 显示游戏
+function showGames(){
+    gameList.innerHTML = '';
+    games.forEach(game => {
+        const gameCard = document.createElement("div");
+        gameCard.classList.add("game-card");
+        gameCard.innerHTML = `
+            <h3>${game.workName}</h3>
+            <p>类型：${game.category}</p>
+            <p>价格：${game.price} 元</p>
+            <p>${game.description}</p>
+            <button>查看详情</button>
+        `;
+        gameList.appendChild(gameCard);   // appendChild表示给这个元素添加一个子元素
+    });
+}
+// 初始化游戏大厅
+showGames();
